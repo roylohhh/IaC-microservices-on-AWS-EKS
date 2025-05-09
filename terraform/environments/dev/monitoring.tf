641eead11fea4dec8e08665401be12448b31b542
Roy Loh <royloh.2002@gmail.com>
2025-05-09 23:54:41 +1000

    modified monitoring.tf

--- terraform/environments/dev/monitoring.tf
+++ terraform/environments/dev/monitoring.tf
@@ -1,37 +1,29 @@
-module "kube_prometheus_stack" {
-  source  = "terraform-module/kube-prometheus-stack/helm"
-  version = "2.6.0"
-
-  name             = "kube-prometheus"
-  namespace        = "monitoring"
+resource "helm_release" "kube_prometheus_stack" {
+  name       = "kube-prometheus"
+  namespace  = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "58.5.0"
   create_namespace = true
-  chart_version    = "58.5.0"
 
-  values = [
-    yamlencode({
-      grafana = {
-        enabled = true
-        service = {
-          type = "ClusterIP"
-        }
-        ingress = {
-          enabled = true
-          annotations = {
-            "kubernetes.io/ingress.class" = "alb"
-          }
-          hosts = ["grafana.fake.com"]
-        }
-      }
-      prometheus = {
-        prometheusSpec = {
-          serviceMonitorSelectorNilUsesHelmValues = false
-        }
-        serviceMonitor = {
-          enabled = true
-        }
-      }
-    })
+  values = [<<-EOF
+    grafana:
+      enabled: true
+      service:
+        type: ClusterIP
+      ingress:
+        enabled: true
+        annotations:
+          kubernetes.io/ingress.class: alb
+        hosts:
+          - grafana.fake.com
+    prometheus:
+      prometheusSpec:
+        serviceMonitorSelectorNilUsesHelmValues: false
+      serviceMonitor:
+        enabled: true
+  EOF
   ]
 
-  depends_on = [module.eks] 
+  depends_on = [module.eks]
 }
